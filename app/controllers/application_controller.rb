9341754d18af3a90671011929a8da0beb691485c
Sheena Sang <sheenasany@gmail.com>
2022-09-12 00:59:42 -0400

    commit tree 50225d852f296f395c72388ac6e346a2e2f0918f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do 
+    bakery = Bakery.all 
+    #returns an array of JSON objects for all bakeries in the database
+    bakery.to_json
+  end
 
+  get '/bakeries/:id' do
+    # returns a single bakery as JSON with its baked goods nested
+    bakery = Bakery.find(params[:id])
+    bakery.to_json({include: :baked_goods})
+  end
+
+  get '/baked_goods/by_price' do
+    # see the class for the method 
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # see the class for the method
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
 end
